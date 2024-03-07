3f510a9878d2c3650bffc67b0caba27931b71ac0
doduyhao2002@gmail.com <doduyhao2002@gmail.com>
2024-03-07 09:57:19 +0700

    UI one post

---
+++ lib/controller/HomeController.dart
@@ -0,0 +1,61 @@
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../model/PostModel.dart';
+import '../service/API_Post.dart';
+
+class HomeController extends GetxController {
+  static HomeController instance = Get.find();
+  RxList<PostModel> listPost = List<PostModel>.empty(growable: true).obs;
+  RxBool isloaded = false.obs;
+  RxBool isliked = false.obs;
+  RxInt postid = 0.obs;
+
+
+  void loadPost() async
+  {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      isloaded(true);
+      final userId = prefs.getInt('id') ?? 0;
+      final token = prefs.getString('token') ?? "";
+      List<PostModel>? result = await API_Post.LoadMainHome(userId, token);
+      if (result != null) {
+        listPost.clear();
+        listPost.addAll(result);
+        update();
+      }
+    }
+    finally {
+      isloaded(false);
+    }
+  }
+
+  // void Like() async
+  // {
+  //   try {
+  //     final prefs = await SharedPreferences.getInstance();
+  //
+  //     isliked(true);
+  //     final userId = prefs.getInt('id') ?? 0;
+  //     final token = prefs.getString('token') ?? "";
+  //     await API_Post.Liked(token, postid.value, userId);
+  //   }
+  //   finally {
+  //     isloaded(false);
+  //   }
+  // }
+
+  // void DeletePost() async
+  // {
+  //   try {
+  //     final prefs = await SharedPreferences.getInstance();
+  //     final userId = prefs.getInt('id') ?? 0;
+  //     final token = prefs.getString('token') ?? "";
+  //     API_Post.deletePost(postid.value, token);
+  //   }
+  //   finally {
+  //     isloaded(false);
+  //   }
+  // }
+}
