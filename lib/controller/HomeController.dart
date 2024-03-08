1070a34f6e7cd0b58bb0ffa78dab15f4e704fba6
Buu Quyen Tran <tranbuuquyen2002@gmail.com>
2024-03-08 21:46:22 +0700

    lazy loading, load theo khoa, giao dien trang create question

--- lib/controller/HomeController.dart
+++ lib/controller/HomeController.dart
@@ -7,6 +7,7 @@ import '../service/API_Post.dart';
 class HomeController extends GetxController {
   static HomeController instance = Get.find();
   RxList<PostModel> listPost = List<PostModel>.empty(growable: true).obs;
+  RxList<PostModel> top10Post = List<PostModel>.empty(growable: true).obs;
   RxBool isloaded = false.obs;
   RxBool isliked = false.obs;
   RxInt postid = 0.obs;
@@ -58,4 +59,23 @@ class HomeController extends GetxController {
   //     isloaded(false);
   //   }
   // }
+
+  void load10HotPost() async
+  {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      isloaded(true);
+      final userId = prefs.getInt('id') ?? 0;
+      final token = prefs.getString('token') ?? "";
+      List<PostModel>? result = await API_Post.LoadTop10(userId, token);
+      if (result != null) {
+        top10Post.clear();
+        top10Post.addAll(result);
+        update();
+      }
+    }
+    finally {
+      isloaded(false);
+    }
+  }
 }
