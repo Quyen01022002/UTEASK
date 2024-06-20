5f1240a1a65e0c45b70f1d2e8ce684609402e3a4
Buu Quyen Tran <tranbuuquyen2002@gmail.com>
2024-06-20 21:28:52 +0700

    Chat Bot, Deliver Questions

--- lib/service/API_Post.dart
+++ lib/service/API_Post.dart
@@ -50,7 +50,7 @@ class API_Post {
 
   static Future<String?> DeliverKhoa(String questions) async {
     final response = await http.post(
-      Uri.parse('http://192.168.40.92:8081/classify'),
+      Uri.parse('http://192.168.178.92:8081/classify'),
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
       },
